5300a49c67a0e1a583b7df1713c754a9c681be6a
Randal Krebs <randal.t.krebs@gmail.com>
2021-03-14 15:03:08 -0300

    cors

--- backend/server.js
+++ backend/server.js
@@ -3,10 +3,11 @@ const path = require("path");
 const express = require("express");
 const connectDB = require("./config/db");
 const productRoutes = require("./routes/productRoutes");
-
+const cors = require("cors");
 connectDB();
 
 const app = express();
+const cors = require("cors");
 
 app.use(express.json());
 
@@ -16,17 +17,17 @@ app.get("/", (req, res) => {
 
 app.use("/api/products", productRoutes);
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/frontend/build")));
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static(path.join(__dirname, "/frontend/build")));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "frontend", "build", "index.html"));
-  });
-} else {
-  app.get("/", (req, res) => {
-    res.send("Api running");
-  });
-}
+//   app.get("*", (req, res) => {
+//     res.sendFile(path.join(__dirname, "frontend", "build", "index.html"));
+//   });
+// } else {
+//   app.get("/", (req, res) => {
+//     res.send("Api running");
+//   });
+// }
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
