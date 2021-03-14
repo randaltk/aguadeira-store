2cb9ea9eb978c7df92fdf038ecc99239f9765678
Randal Krebs <randal.t.krebs@gmail.com>
2021-03-14 13:55:31 -0300

    first

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,8 @@
-require("dotenv").config();
+require("dotenv").config({ path: "./config.env" });
+const path = require("path");
 const express = require("express");
-const productRoutes = require("./routes/productRoutes");
 const connectDB = require("./config/db");
+const productRoutes = require("./routes/productRoutes");
 
 connectDB();
 
@@ -14,6 +15,18 @@ app.get("/", (req, res) => {
 });
 
 app.use("/api/products", productRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/build")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "frontend", "build", "index.html"));
+  });
+} else {
+  app.get("/", (req, res) => {
+    res.send("Api running");
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
