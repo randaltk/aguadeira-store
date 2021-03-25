1e5f087b4cdd3e69dcc8e37715715e0f6b7bdda4
Randal Krebs <randal.t.krebs@gmail.com>
2021-03-25 02:12:41 -0300

    about

--- frontend/src/screens/AboutScreen.js
+++ frontend/src/screens/AboutScreen.js
@@ -1,6 +1,6 @@
-import "./About.css";
-import Maozinha from "../assets/images/About/maozinha.PNG";
-import Matinho from "../assets/images/About/matinho.PNG";
+import "./AboutScreen.css";
+import Maozinha from "../assets/images/About/maozinha.png";
+import Matinho from "../assets/images/About/matinho.png";
 import Mandala from "../assets/images/About/mandala.PNG";
 import Coelho from "../assets/images/About/coelin.PNG";
 import Folhas from "../assets/images/About/folhas.PNG";
@@ -9,8 +9,60 @@ const AboutScreen = () => {
   return (
     <>
       <div className="about__wrapper">
-        <h1 className="about__h1">Conheça...</h1>
-        <div className="grid__container">
+        <h1 className="about__h1">Sobre a Marca</h1>
+        <p className="about__dots">••••••••</p>
+        <h1 className="matinho__h1">Natural</h1>
+        <div className="matinho__wrapper">
+          <img className="matinho__img" alt="matinho" src={Matinho}></img>
+          <p className="matinho__desc">
+            {" "}
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. In nisi
+            purus, tempor eget vestibulum facilisis, volutpat sed dolor. Vivamus
+            felis justo, ornare vel ornare pellentesque, interdum id est. Mauris
+            elementum sem quis tortor iaculis, sit amet pretium dolor interdum.
+            Pellentesque iaculis sed nibh vel vestibulum. Vestibulum gravida
+            egestas sapien ut vestibulum. Nam a rhoncus arcu. Aliquam euismod,
+            odio semper faucibus volutpat, risus nulla laoreet est, quis lacinia
+            enim ex efficitur lorem. Donec ut consectetur est. Pellentesque non
+            dignissim nibh. Nam vulputate consectetur quam sit amet condimentum.
+            Proin interdum lectus at luctus congue.
+          </p>
+        </div>
+        <h1 className="maozinha__h1">Artesanal</h1>
+        <div className="maozinha__wrapper">
+          <p className="maozinha__desc">
+            {" "}
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. In nisi
+            purus, tempor eget vestibulum facilisis, volutpat sed dolor. Vivamus
+            felis justo, ornare vel ornare pellentesque, interdum id est. Mauris
+            elementum sem quis tortor iaculis, sit amet pretium dolor interdum.
+            Pellentesque iaculis sed nibh vel vestibulum. Vestibulum gravida
+            egestas sapien ut vestibulum. Nam a rhoncus arcu. Aliquam euismod,
+            odio semper faucibus volutpat, risus nulla laoreet est, quis lacinia
+            enim ex efficitur lorem. Donec ut consectetur est. Pellentesque non
+            dignissim nibh. Nam vulputate consectetur quam sit amet condimentum.
+            Proin interdum lectus at luctus congue.
+          </p>
+
+          <img className="maozinha__img" alt="maozinha" src={Maozinha}></img>
+        </div>
+
+        {/*    <div className="grid__container">
+          <img className="grid__img" alt="matinho" src={Matinho}></img>
+          <p className="grid__desc">
+            {" "}
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. In nisi
+            purus, tempor eget vestibulum facilisis, volutpat sed dolor. Vivamus
+            felis justo, ornare vel ornare pellentesque, interdum id est. Mauris
+            elementum sem quis tortor iaculis, sit amet pretium dolor interdum.
+            Pellentesque iaculis sed nibh vel vestibulum. Vestibulum gravida
+            egestas sapien ut vestibulum. Nam a rhoncus arcu. Aliquam euismod,
+            odio semper faucibus volutpat, risus nulla laoreet est, quis lacinia
+            enim ex efficitur lorem. Donec ut consectetur est. Pellentesque non
+            dignissim nibh. Nam vulputate consectetur quam sit amet condimentum.
+            Proin interdum lectus at luctus congue.
+          </p>
+
           <img className="grid__img" alt="maozinha" src={Maozinha}></img>
 
           <p className="grid__desc">
@@ -26,22 +78,6 @@ const AboutScreen = () => {
             dignissim nibh. Nam vulputate consectetur quam sit amet condimentum.
             Proin interdum lectus at luctus congue.{" "}
           </p>
-
-          <p className="grid__desc">
-            {" "}
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. In nisi
-            purus, tempor eget vestibulum facilisis, volutpat sed dolor. Vivamus
-            felis justo, ornare vel ornare pellentesque, interdum id est. Mauris
-            elementum sem quis tortor iaculis, sit amet pretium dolor interdum.
-            Pellentesque iaculis sed nibh vel vestibulum. Vestibulum gravida
-            egestas sapien ut vestibulum. Nam a rhoncus arcu. Aliquam euismod,
-            odio semper faucibus volutpat, risus nulla laoreet est, quis lacinia
-            enim ex efficitur lorem. Donec ut consectetur est. Pellentesque non
-            dignissim nibh. Nam vulputate consectetur quam sit amet condimentum.
-            Proin interdum lectus at luctus congue.
-          </p>
-
-          <img className="grid__img" alt="matinho" src={Matinho}></img>
 
           <img className="grid__img" alt="mandala" src={Mandala}></img>
 
@@ -91,6 +127,7 @@ const AboutScreen = () => {
             Proin interdum lectus at luctus congue.{" "}
           </p>
         </div>
+        */}
       </div>
     </>
   );
