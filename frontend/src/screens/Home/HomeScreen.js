998532c60d9e8dd769238a65c33afd75652447a5
Randal Krebs <randal.t.krebs@gmail.com>
2021-03-31 17:32:25 -0300

    att

--- frontend/src/screens/Home/HomeScreen.js
+++ frontend/src/screens/Home/HomeScreen.js
@@ -1,7 +1,6 @@
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 
-
 import depoimentos from "../../assets/images/depoimentos.jpg";
 import { Link } from "react-router-dom";
 import "./HomeScreen.css";
@@ -12,6 +11,7 @@ import Footer from "../../components/Footer/Footer";
 
 //Actions
 import { getProducts as listProducts } from "../../redux/actions/productActions";
+import Product from "../../components/Product/Product";
 
 const HomeScreen = () => {
   const dispatch = useDispatch();
@@ -28,38 +28,44 @@ const HomeScreen = () => {
       <div className="capa__wrapper">
         <h1 className="destaquesh1">Destaques</h1>
         <p className="pages__line"></p>
-        <div className="destaques__wrapper">
-          <div className="blog__post1" id="fit">
-            {products.slice(0, 4).map((product) => (
-              <div className="left__image">
-                <img src={product.imageUrl} alt={product.name} />
+        <div className="destaques__products">
+          {products.slice(0, 4).map((product) => (
+            <div className="destaques__img">
+              <img src={product.imageUrl} alt={product.name} />
+              <div style={{ textAlign: "center" }}>
+                <p>{product.name}</p>
+                <p>{product.price}</p>
               </div>
-            ))}
-          </div>
-
+            </div>
+          ))}
+        </div>
+        <h1 className="destaquesh1" id="post2h2">
+          Lançamentos
+        </h1>
+        <p className="pages__line"></p>
+        <div className="destaques__products">
+          {products.slice(2, 6).map((product) => (
+            <div className="destaques__img">
+              <img src={product.imageUrl} alt={product.name} />
+              <div style={{ textAlign: "center" }}>
+                <p>{product.name}</p>
+                <p>{product.price}</p>
+              </div>
+            </div>
+          ))}
+        </div>
+        <Link to="/about" style={{ textDecoration: "none" }}>
+          {" "}
           <h1 className="destaquesh1" id="post2h2">
-            Lançamentos
+            Conheça a marca
           </h1>
-          <p className="pages__line"></p>
-          <div className="blog__post1">
-            {products.slice(2, 6).map((product) => (
-              <div className="left__image">
-                <img src={product.imageUrl} alt={product.name} />
-              </div>
-            ))}
-          </div>
-          <Link to="/about" style={{ textDecoration: "none" }}>
-            {" "}
-            <h1 className="destaquesh1" id="post2h2">
-              Conheça a marca
-            </h1>
-          </Link>
+        </Link>
 
-          <p className="pages__line"></p>
-          <br />
-          <br />
+        <p className="pages__line"></p>
+        <br />
+        <br />
 
-          {/*    <p>~ thinking about seven generations ahead</p>
+        {/*    <p>~ thinking about seven generations ahead</p>
           <br />
           <p>
             {" "}
@@ -68,29 +74,29 @@ const HomeScreen = () => {
             mulheres, com muita magia e amor.
           </p>
           */}
-          <div className="blog__post1">
-            <Link to="/about" style={{ textDecoration: "none" }}>
-              <img
-                className="img__post1"
-                src={depoimentos}
-                alt="cesta-sabonetes"
-              />
-            </Link>
+        <div className="blog__post1">
+          <Link to="/about" style={{ textDecoration: "none" }}>
+            <img
+              className="img__post1"
+              src={depoimentos}
+              alt="cesta-sabonetes"
+            />
+          </Link>
 
-            <br />
+          <br />
 
-            <p className="maozinha__desc" id="depoimentodesc">
-              “Os cosméticos naturais da aguadeira possuem o poder das plantas e
-              são feitos à mão com amor e dedicação. São alquimias mágicas e
-              únicas! Ao comprar de pequenos produtores de cosméticos naturais
-              você não só está ajudando sonhos a se realizarem, como também
-              desestimula grandes empresas a continuarem a produzir cosméticos
-              poluentes e prejudiciais à saúde..” <br></br>
-            </p>
-          </div>
+          <p className="maozinha__desc" id="depoimentodesc">
+            “Os cosméticos naturais da aguadeira possuem o poder das plantas e
+            são feitos à mão com amor e dedicação. São alquimias mágicas e
+            únicas! Ao comprar de pequenos produtores de cosméticos naturais
+            você não só está ajudando sonhos a se realizarem, como também
+            desestimula grandes empresas a continuarem a produzir cosméticos
+            poluentes e prejudiciais à saúde..” <br></br>
+          </p>
         </div>
-        <Instagram />
-      </div>{" "}
+      </div>
+      <Instagram />
+
       <Footer2 />
       <Footer />
     </>
